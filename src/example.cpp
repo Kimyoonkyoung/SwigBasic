b81306418719a19d997fc1d9af9a2a45ec009d28
Rachel <rachel.minii@kakaocorp.com>
2018-01-02 16:50:31 +0900

    [Add] find Python Libs add for JSON work
    jsoncpp lib를 링크하기 위해서 Python lib 찾는 명령어 추가 및 예제 추가

--- src/example.cpp
+++ src/example.cpp
@@ -8,6 +8,9 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
+#include <json/json.h>
+#include <fstream>
+
 #include "example.h"
 
 example::example()
@@ -22,6 +25,28 @@ example::~example()
 
 void example::lib_cpp_hello()
 {
+    Json::Value event;
+    Json::Value vec(Json::arrayValue);
+    vec.append(Json::Value(1));
+    vec.append(Json::Value(2));
+    vec.append(Json::Value(3));
+
+    event["competitors"]["home"]["name"] = "Liverpool";
+    event["competitors"]["away"]["code"] = 89223;
+    event["competitors"]["away"]["name"] = "Aston Villa";
+    event["competitors"]["away"]["code"]=vec;
+
+    //std::cout << event << std::endl;
+
+    Json::StyledWriter styledWriter;
+    std::string strJSON = styledWriter.write(event);
+
+    std::ofstream file;
+    file.open("/Users/kakao/Desktop/test.json");
+
+    file << strJSON;
+    file.close();
+
     std::cout << "Hello from lib_cpp" << std::endl;
 
     cv::Mat test = cv::imread("/Users/kakao/Desktop/test.png");
